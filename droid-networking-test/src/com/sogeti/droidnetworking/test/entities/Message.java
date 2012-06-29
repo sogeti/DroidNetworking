fe28cb76acadda1a54123f15e5af41ca75a2528a
Martin Dahl <martin.dahl@sogeti.se>
2012-06-29 14:15:13 +0200

    Renamed libray

---
+++ droid-networking-test/src/com/sogeti/droidnetworking/test/entities/Message.java
@@ -0,0 +1,40 @@
+package com.sogeti.droidnetworking.test.entities;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Message {
+    private int id;
+    private String title;
+    private String body;
+    
+    public Message(JSONObject jsonObject) throws JSONException {
+        this.id = jsonObject.getInt("id");
+        this.title = jsonObject.getString("title");
+        this.body = jsonObject.getString("body");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
