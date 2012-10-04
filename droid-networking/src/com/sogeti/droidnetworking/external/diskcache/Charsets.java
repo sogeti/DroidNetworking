d19b3b130e304deaaa4935055688c2e10d8e105e
Martin Dahl <martin.dahl@sogeti.se>
2012-10-04 13:45:11 +0200

    Added JakeWharton DiskLruCache and test cases

---
+++ droid-networking/src/com/sogeti/droidnetworking/external/diskcache/Charsets.java
@@ -0,0 +1,9 @@
+package com.sogeti.droidnetworking.external.diskcache;
+
+import java.nio.charset.Charset;
+
+/** From java.nio.charset.Charsets */
+public class Charsets {
+	public static final Charset US_ASCII = Charset.forName("US-ASCII");
+	public static final Charset UTF_8 = Charset.forName("UTF-8");
+}
